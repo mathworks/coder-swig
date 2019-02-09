be46324d8e9526b2392ab466a13a2661bc31559a
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2019-02-09 18:24:14 -0500

    Indent

--- 03-timestwo-dynamic-size/timestwoMain.cs
+++ 03-timestwo-dynamic-size/timestwoMain.cs
@@ -1,53 +1,56 @@
 public class timestwoMain {
-   public static void printArray(doubleArray d, int n) {
-       if (n == 0) {
-           System.Console.WriteLine("Empty array");
-           return;
-       }
-       for (int i = 0; i < n-1; i++) {
-           System.Console.WriteLine(d.getitem(i) + ", ");
-       }
-       System.Console.WriteLine(d.getitem(n-1));
-
-   }
+    public static void printArray(doubleArray d, int n) {
+        if (n == 0) {
+            System.Console.WriteLine("Empty array");
+            return;
+        }
+        for (int i = 0; i < n-1; i++) {
+            System.Console.WriteLine(d.getitem(i) + ", ");
+        }
+        System.Console.WriteLine(d.getitem(n-1));
+    }
 
     // Test Driver
-   public static void Main(string[] args) {
-      System.Console.WriteLine("Calling initialize");
-      timestwocs.timestwo_initialize();
-      // 2-by-3 input
-      int m = 2;
-      int n = 3;
+    public static void Main(string[] args) {
+        // Call initialize function to set up any necessary state
+        System.Console.WriteLine("Calling initialize");
+        timestwocs.timestwo_initialize();
 
-      using (intArray dims = new intArray(2)) // Size vector
-      using (doubleArray data = new doubleArray(6)) // Data
-      using (emxArray_real_T x = timestwocs.emxCreateWrapper_real_T(data.cast(),m,n)) // Input
-      using (emxArray_real_T y = timestwocs.emxCreate_real_T(0,0)) {  // Output
-          // 2-by-3 input
-          dims.setitem(0, m);
-          dims.setitem(1, n);
+        // 2-by-3 input
+        int m = 2;
+        int n = 3;
 
-          // Data
-          int numel = m*n;
-          for (int i = 0; i < numel; i++) {
-              data.setitem(i,i);
-          }
-          System.Console.WriteLine("Initial data");
-          timestwoMain.printArray(data,numel);
-          // Call entry-point
-          timestwocs.timestwo(x,y);
+        using (intArray dims = new intArray(2)) // Size vector
+        using (doubleArray data = new doubleArray(6)) // Data
+        using (emxArray_real_T x = timestwocs.emxCreateWrapper_real_T(data.cast(),m,n)) // Input
+        using (emxArray_real_T y = timestwocs.emxCreate_real_T(0,0)) {  // Output
+            // 2-by-3 input
+            dims.setitem(0, m);
+            dims.setitem(1, n);
 
-          // Gather returned data
-          doubleArray output = doubleArray.frompointer(y.data);
-          int outputND = y.numDimensions;
-          intArray outputSize = intArray.frompointer(y.size);
-          int outputNumel = 1;
-          for (int dim = 0; dim < outputND; dim++) {
-              outputNumel *= outputSize.getitem(dim);
-          }
-          timestwoMain.printArray(output,outputNumel);
-          System.Console.WriteLine("Calling terminate");
-          timestwocs.timestwo_terminate();
-      }
-   }
+            // Data
+            int numel = m*n;
+            for (int i = 0; i < numel; i++) {
+                data.setitem(i,i);
+            }
+            System.Console.WriteLine("Initial data");
+            timestwoMain.printArray(data,numel);
+            // Call entry-point
+            timestwocs.timestwo(x,y);
+
+            // Gather returned data
+            doubleArray output = doubleArray.frompointer(y.data);
+            int outputND = y.numDimensions;
+            intArray outputSize = intArray.frompointer(y.size);
+            int outputNumel = 1;
+            for (int dim = 0; dim < outputND; dim++) {
+                outputNumel *= outputSize.getitem(dim);
+            }
+            timestwoMain.printArray(output,outputNumel);
+
+            // Call terminate function to perform any necessary clean up
+            System.Console.WriteLine("Calling terminate");
+            timestwocs.timestwo_terminate();
+        }
+    }
 }
--- 03-timestwo-dynamic-size/timestwoMain.java
+++ 03-timestwo-dynamic-size/timestwoMain.java
@@ -1,64 +1,67 @@
 import java.lang.System;
 import timestwoJava.*;
 public class timestwoMain {
-   static {
-      System.loadLibrary("timestwo"); // Load native library at runtime
-                                 // timestwo.dll (Windows) or libtimestwo.so (Unixes)
-   }
- 
-   public static void printArray(doubleArray d, int n) {
-       if (n == 0) {
-           System.out.println("Empty array");
-           return;
-       }
-       for (int i = 0; i < n-1; i++) {
-           System.out.println(d.getitem(i) + ", ");
-       }
-       System.out.println(d.getitem(n-1));
-       
-   }
+    static {
+        System.loadLibrary("timestwo"); // Load native library at runtime
+        // timestwo.dll (Windows) or libtimestwo.so (Unixes)
+    }
+
+    public static void printArray(doubleArray d, int n) {
+        if (n == 0) {
+            System.out.println("Empty array");
+            return;
+        }
+        for (int i = 0; i < n-1; i++) {
+            System.out.println(d.getitem(i) + ", ");
+        }
+        System.out.println(d.getitem(n-1));
+
+    }
 
     // Test Driver
-   public static void main(String[] args) {
-      System.out.println("Calling initialize");
-      timestwo.timestwo_initialize();
-      
-      // 2-by-3 input
-      int m = 2;
-      int n = 3;
-      intArray dims = new intArray(2);
-      dims.setitem(0, m);
-      dims.setitem(1, n);
-      
-      // Data
-      int numel = m*n;
-      doubleArray data = new doubleArray(6);
-      for (int i = 0; i < numel; i++) {
-          data.setitem(i,i);
-      }
-      System.out.println("Initial data");
-      timestwoMain.printArray(data,numel);
-      // Create input
-      emxArray_real_T x = timestwo.emxCreateWrapper_real_T(data.cast(),m,n);
-      
-      // Create output
-      emxArray_real_T y = timestwo.emxCreate_real_T(0,0);
-      
-      // Call entry-point
-      timestwo.timestwo(x,y);
-      
-      // Gather returned data
-      doubleArray output = doubleArray.frompointer(y.getData());
-      int outputND = y.getNumDimensions();
-      intArray outputSize = intArray.frompointer(y.getSize());
-      int outputNumel = 1;
-      for (int dim = 0; dim < outputND; dim++) {
-          outputNumel *= outputSize.getitem(dim);
-      }
-      timestwoMain.printArray(output,outputNumel);
-      System.out.println("Calling terminate");
-      timestwo.timestwo_terminate();
+    public static void main(String[] args) {
+        // Call initialize function to set up any necessary state
+        System.out.println("Calling initialize");
+        timestwo.timestwo_initialize();
 
-      // No need to clean up as destructors are automatically called
-   }
+        // 2-by-3 input
+        int m = 2;
+        int n = 3;
+        intArray dims = new intArray(2);
+        dims.setitem(0, m);
+        dims.setitem(1, n);
+
+        // Data
+        int numel = m*n;
+        doubleArray data = new doubleArray(6);
+        for (int i = 0; i < numel; i++) {
+            data.setitem(i,i);
+        }
+        System.out.println("Initial data");
+        timestwoMain.printArray(data,numel);
+        // Create input
+        emxArray_real_T x = timestwo.emxCreateWrapper_real_T(data.cast(),m,n);
+
+        // Create output
+        emxArray_real_T y = timestwo.emxCreate_real_T(0,0);
+
+        // Call entry-point
+        timestwo.timestwo(x,y);
+
+        // Gather returned data
+        doubleArray output = doubleArray.frompointer(y.getData());
+        int outputND = y.getNumDimensions();
+        intArray outputSize = intArray.frompointer(y.getSize());
+        int outputNumel = 1;
+        for (int dim = 0; dim < outputND; dim++) {
+            outputNumel *= outputSize.getitem(dim);
+        }
+        timestwoMain.printArray(output,outputNumel);
+
+        // Call terminate function to perform any necessary clean up
+        System.out.println("Calling terminate");
+        timestwo.timestwo_terminate();
+
+        // No need to clean up as destructors are automatically called
+    }
 }
